659e7b4ca9917fe4c7a8dadc25751df77e5591c3
Young Seok Tony Kim <mightydeveloper@gmail.com>
2015-10-06 20:41:04 +0900

    Initial Commit

---
+++ MIPS Assembler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MIPS Assembler
+//
+//  Created by Young Seok Kim on 10/6/15.
+//  Copyright © 2015 TonyKim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
